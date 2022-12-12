737ba150796080210a7e81ca4df4850c7585c5b6
RIcardo Batel <ricardo.batel@hotmail.com>
2022-12-12 20:54:00 -0300

    Marcando tarefas realizadas

--- tarefa.service.php
+++ tarefa.service.php
@@ -51,6 +51,15 @@ class TarefaService {
 		$stmt->execute();
 
 	}
+
+	public function marcarRealizada() { //update
+
+		$query = "update tb_tarefas set id_status = ? where id = ?";
+		$stmt = $this->conexao->prepare($query);
+		$stmt->bindValue(1, $this->tarefa->__get('id_status'));
+		$stmt->bindValue(2, $this->tarefa->__get('id'));
+		return $stmt->execute(); 
+	}
 }
 
 ?>
